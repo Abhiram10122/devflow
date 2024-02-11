6dd1ad55a88c8fca81a4f7163bd069d41cf48cf1
Abhiram <vemulaabhiram2004@gmail.com>
2024-02-11 11:45:52 +0530

    Added authentication using clerk

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="h1-bold">NextJs 14 we&apos;re coming!</h1>
-      <h2 className="h2-bold">NextJs 14 we&apos;re coming!</h2>
-    </main>
-  );
-}
