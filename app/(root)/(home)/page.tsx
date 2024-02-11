3310abb2f8b3164f214959d77ed31c122122ef3d
Abhiram <vemulaabhiram2004@gmail.com>
2024-02-11 16:21:03 +0530

    Implemented Desktop Nav and Theme changer

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <main>
-      <UserButton afterSignOutUrl="/" />
-    </main>
-  );
+  return <main></main>;
 }
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -10,20 +10,24 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState("light");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
-    // handleThemeChange();
+    handleThemeChange();
   }, [mode]);
 
   return (
